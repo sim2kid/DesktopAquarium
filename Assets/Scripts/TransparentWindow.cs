9711b7a1cc81ae7e5ca47d1f0c829160c4578dcb
sim2kid <sim2kid@users.noreply.github.com>
2021-05-16 22:25:29 -0500

    Initial Commit after Unity

---
+++ Assets/Scripts/TransparentWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    private struct MARGINS {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+
+    private void Start()
+    {
+        //MessageBox(new IntPtr(0), "Hello World!", "Hello Caption!", 0);
+        IntPtr hWnd = GetActiveWindow();
+
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 };
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+    }
+}
