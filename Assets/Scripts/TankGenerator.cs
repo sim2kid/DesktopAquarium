8b99c48a2684ba6f4b6c91dc8345c2b1ff9d78c3
sim2kid <sim2kid@users.noreply.github.com>
2021-05-18 21:46:41 -0500

    Created a tank generator class

---
+++ Assets/Scripts/TankGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesktopAquarium
+{
+    public class TankGenerator : MonoBehaviour
+    {
+        Camera camToMonitor;
+
+        public void AssignCamera(Camera toAssign) 
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
