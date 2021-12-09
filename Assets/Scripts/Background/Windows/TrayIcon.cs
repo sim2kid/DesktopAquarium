00cb16711f269414106063eea9267a4ab8bea3fc
sim2kid <sim2kid@users.noreply.github.com>
2021-12-08 18:35:33 -0600

    Adds system tray icon code

--- Assets/Scripts/Background/Windows/TrayIcon.cs
+++ Assets/Scripts/Background/Windows/TrayIcon.cs
@@ -1,18 +1,47 @@
-using System;
 using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace Background.Windows
 {
-    public class TrayIcon
+    public class TrayIcon : MonoBehaviour
     {
+        public string ICOPath;
+        public UnityEvent onClick;
+
         private NotifyIcon notifyIcon;
-        private System.ComponentModel.IContainer components;
 
-        public TrayIcon() 
+
+        private void OnEnable()
         {
-            components = new System.ComponentModel.Container();
+            notifyIcon = new NotifyIcon();
 
-            notifyIcon = new NotifyIcon(components);
+            if (!string.IsNullOrWhiteSpace(ICOPath))
+            {
+                string p = ToBackslash(System.IO.Path.Combine(UnityEngine.Application.streamingAssetsPath, ICOPath));
+                notifyIcon.Icon = new System.Drawing.Icon(p);
+            }
+
+            notifyIcon.Text = $"{UnityEngine.Application.productName}";
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += new System.EventHandler(this.DoubleClick);
+        }
+
+        private void OnDisable()
+        {
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+
+        private void DoubleClick(object Sender, System.EventArgs e) 
+        {
+            Debug.Log("Click!");
+            onClick.Invoke();
+        }
+
+        private string ToBackslash(string s) 
+        {
+            return s.Replace('/', '\\');
         }
     }
 }
